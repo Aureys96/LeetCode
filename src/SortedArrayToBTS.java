5f6c6bab2e88f8c1eaa69b47ae32f738798f5801
Kostyan <konstantingolubev8@gmail.com>
2021-11-10 00:44:53 +0300

    toString for TreeNode implemented + minor SortedArrayToBTS fix

--- src/SortedArrayToBTS.java
+++ src/SortedArrayToBTS.java
@@ -3,14 +3,14 @@ import helpers.TreeNode;
 public class SortedArrayToBTS {
 
     public static void main(String[] args) {
-
+        System.out.println(new SortedArrayToBTS().sortedArrayToBST(new int[]{0,1,2,3,4,5}));
     }
 
     public TreeNode sortedArrayToBST(int[] nums) {
         if (nums.length == 1) {
             return new TreeNode(nums[0]);
         }
-        int mid_index = nums[(nums.length - 1) / 2];
+        int mid_index = (nums.length - 1) / 2;
         int i = 0;
         TreeNode prev = new TreeNode(nums[i++]);
         while (i < mid_index) {
--- src/helpers/TreeNode.java
+++ src/helpers/TreeNode.java
@@ -2,6 +2,7 @@ package helpers;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Set;
 
 public class TreeNode {
     public int val;
@@ -9,17 +10,15 @@ public class TreeNode {
     public TreeNode right;
 
     public static void main(String[] args) {
-        TreeNode root = new TreeNode(15);
-        TreeNode left = new TreeNode(10);
-        TreeNode right = new TreeNode(25);
-        TreeNode rightRight = new TreeNode(45);
-        TreeNode rightLeft = new TreeNode(39);
-        TreeNode leftRight = new TreeNode(-255);
+        TreeNode root = new TreeNode(0);
+        TreeNode left = new TreeNode(-3);
+        TreeNode right = new TreeNode(9);
+        TreeNode rightLeft = new TreeNode(5);
+        TreeNode leftLeft = new TreeNode(-10);
         root.left = left;
         root.right = right;
-        root.right.right = rightRight;
         root.right.left = rightLeft;
-        root.left.right = leftRight;
+        root.left.left = leftLeft;
 
         System.out.println(root);
     }
@@ -53,6 +52,17 @@ public class TreeNode {
             q.offer(node.right);
         }
 
-        return sb.toString();
+        int lastNumberIndex = 0;
+        final String answer = sb.toString();
+        final char[] chars = answer.toCharArray();
+        Set<Character> digits = Set.of('0', '1', '2', '3', '4', '5', '6', '7', '8', '9');
+        for (int i = answer.length() - 1; i > -1; i--) {
+            if (digits.contains(chars[i])) {
+                lastNumberIndex = i;
+                break;
+            }
+        }
+
+        return answer.substring(0, lastNumberIndex + 1);
     }
 }
