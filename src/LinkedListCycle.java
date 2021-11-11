c5c0ff75acdfa85bc0d06dc4688399ebe9d0042e
Kostyan <konstantingolubev8@gmail.com>
2021-11-11 21:48:03 +0300

    LinkedListCycle

---
+++ src/LinkedListCycle.java
@@ -0,0 +1,29 @@
+public class LinkedListCycle {
+    public boolean hasCycle(ListNode head) {
+        if (head == null)
+            return false;
+        if (head.next == null || head.next.next == null)
+            return false;
+        ListNode slow = head;
+        ListNode fast = head.next.next;
+
+        while (slow.next != null && fast.next != null && fast.next.next != null) {
+            if (slow == fast)
+                return true;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return false;
+    }
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
